c4502d0bc8b63fdece9e6dc99170a922093ebf8a
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-02-17 20:37:18 +0530

    updated version
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- variables.tf
+++ variables.tf
@@ -80,11 +80,11 @@ variable "server_name" {
   default     = "default"
 }
 
-variable "entitlement_key" {
+/*variable "entitlement_key" {
   type        = string
   description = "The entitlement key required to access Cloud Pak images"
   sensitive   = true
-}
+}*/
 
 variable "dbpassword" {
   type        = string
