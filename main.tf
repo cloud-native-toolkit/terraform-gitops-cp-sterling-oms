91a37630788c76c2bbd118dc6234ab800910964e
Yogendra Srivastava <ysrivastava@in.ibm.com>
2022-04-13 22:36:07 +0530

    rbac roles
    Signed-off-by: Yogendra Srivastava <ysrivastava@in.ibm.com>

--- main.tf
+++ main.tf
@@ -290,9 +290,9 @@ module "service_account" {
     ]
     
   }]
-  rbac_roles = [
+  rbac_roles = [{
       "edit"
-    ]
+  }]
   rbac_cluster_scope = true
   server_name = var.server_name
   pull_secrets = ["ibm-entitlement-key"]
